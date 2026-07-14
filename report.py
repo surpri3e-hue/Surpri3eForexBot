f53c0131201ea21d9ffabb5b036bac5a37ab5fba
surpri3e-hue <surpri3e@gmail.com>
2026-07-14 22:13:28 +0330

    Update report.py

--- report.py
+++ report.py
@@ -1,30 +1,98 @@
-from database import get_stats
+from database import get_trades
 
 
 def create_report():
 
-    stats = get_stats()
+    trades = get_trades()
 
-    report = f"""
-📊 Surpri3e Forex Bot Status
+
+    total = len(trades)
+
+    tp = 0
+    sl = 0
+    active = 0
+
+
+
+    for trade in trades:
+
+        result = trade[7]
+
+
+        if result == "TP":
+            tp += 1
+
+        elif result == "SL":
+            sl += 1
+
+        elif result == "ACTIVE":
+            active += 1
+
+
+
+
+    finished = tp + sl
+
+
+    if finished > 0:
+
+        winrate = round(
+            (tp / finished) * 100,
+            2
+        )
+
+    else:
+
+        winrate = 0
+
+
+
+
+    # Profit Factor ساده بر اساس RR 1:2
+
+    profit = tp * 2
+
+    loss = sl * 1
+
+
+    if loss > 0:
+
+        pf = round(
+            profit / loss,
+            2
+        )
+
+    else:
+
+        pf = 0
+
+
+
+
+    return f"""
+📊 Surpri3e AI Report
+
 
 Total Signals:
-{stats['total']}
+{total}
 
-✅ TP Hit:
-{stats['wins']}
 
-❌ SL Hit:
-{stats['losses']}
+TP Hit:
+✅ {tp}
+
+
+SL Hit:
+❌ {sl}
+
+
+Active:
+⏳ {active}
+
 
 Win Rate:
-{stats['winrate']}%
+{winrate}%
+
 
 Profit Factor:
-{stats['profit_factor']}
-
-RR:
-1:2
+{pf}
 """
-
-    return report
