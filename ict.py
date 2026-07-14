6e37408b7cd76394b87bb009c6f94f0004665c84
surpri3e-hue <surpri3e@gmail.com>
2026-07-14 22:54:04 +0330

    Update ict.py

--- ict.py
+++ ict.py
@@ -1,11 +1,4 @@
-def ict_analysis(df):
-
-    last = df.iloc[-1]
-
-    return {
-        "direction": "BUY",
-        "score": 100,
-        "reason": [
-            "TEST SIGNAL"
-        ]
-    }
+return {
+ "direction": "BUY",
+ "score":100
+}
