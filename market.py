5e61ca1a6e6fd4879062a892f6b507ed75177e69
surpri3e-hue <surpri3e@gmail.com>
2026-07-14 21:21:23 +0330

    Update market.py

--- market.py
+++ market.py
@@ -1,1 +1,36 @@
+import os
+import requests
+import pandas as pd
 
+API_KEY = os.getenv("TWELVE_API_KEY")
+
+
+def get_gold_candles(interval):
+    url = "https://api.twelvedata.com/time_series"
+
+    params = {
+        "symbol": "XAU/USD",
+        "interval": interval,
+        "outputsize": 200,
+        "apikey": API_KEY
+    }
+
+    response = requests.get(url, params=params)
+    data = response.json()
+
+    if "values" not in data:
+        return None
+
+    df = pd.DataFrame(data["values"])
+
+    df = df.astype({
+        "open": float,
+        "high": float,
+        "low": float,
+        "close": float
+    })
+
+    # مرتب کردن از قدیمی به جدید
+    df = df.iloc[::-1].reset_index(drop=True)
+
+    return df
