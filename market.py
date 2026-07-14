b4112b1a2303efead451502a7647ff68a31677ec
surpri3e-hue <surpri3e@gmail.com>
2026-07-14 21:45:59 +0330

    Update market.py

--- market.py
+++ market.py
@@ -2,10 +2,12 @@ import os
 import requests
 import pandas as pd
 
+
 API_KEY = os.getenv("TWELVE_API_KEY")
 
 
 def get_gold_candles(interval):
+
     url = "https://api.twelvedata.com/time_series"
 
     params = {
@@ -15,22 +17,51 @@ def get_gold_candles(interval):
         "apikey": API_KEY
     }
 
-    response = requests.get(url, params=params)
-    data = response.json()
+    try:
 
-    if "values" not in data:
+        response = requests.get(
+            url,
+            params=params,
+            timeout=10
+        )
+
+        data = response.json()
+
+
+        if "values" not in data:
+            print("DATA ERROR:", data)
+            return None
+
+
+        df = pd.DataFrame(
+            data["values"]
+        )
+
+
+        df = df.astype({
+            "open": float,
+            "high": float,
+            "low": float,
+            "close": float
+        })
+
+
+        # مرتب‌سازی از قدیمی به جدید
+        df = df.iloc[::-1].reset_index(drop=True)
+
+
+        return df
+
+
+
+    except requests.exceptions.Timeout:
+
+        print("API TIMEOUT")
         return None
 
-    df = pd.DataFrame(data["values"])
 
-    df = df.astype({
-        "open": float,
-        "high": float,
-        "low": float,
-        "close": float
-    })
 
-    # مرتب کردن از قدیمی به جدید
-    df = df.iloc[::-1].reset_index(drop=True)
+    except Exception as e:
 
-    return df
+        print("MARKET ERROR:", e)
+        return None
