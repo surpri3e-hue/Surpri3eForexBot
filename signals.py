046e51dc93ef26bef7b2d67e13c48e7aac833cc3
surpri3e-hue <surpri3e@gmail.com>
2026-07-14 22:06:45 +0330

    Update signals.py

--- signals.py
+++ signals.py
@@ -3,39 +3,84 @@ def create_signal(df, analysis):
     if analysis is None:
         return None
 
+
     last = df.iloc[-1]
 
-    entry = last["close"]
+
+    entry = float(last["close"])
+
+
 
     if analysis["direction"] == "BUY":
 
-        sl = df["low"].iloc[-10:].min()
+
+        # حد ضرر زیر آخرین کف مهم
+
+        sl = float(
+            df["low"].iloc[-15:].min()
+        )
+
 
         risk = entry - sl
 
+
         if risk <= 0:
             return None
+
 
         tp = entry + (risk * 2)
 
 
+
     elif analysis["direction"] == "SELL":
 
-        sl = df["high"].iloc[-10:].max()
+
+        # حد ضرر بالای آخرین سقف مهم
+
+        sl = float(
+            df["high"].iloc[-15:].max()
+        )
+
 
         risk = sl - entry
+
 
         if risk <= 0:
             return None
 
+
         tp = entry - (risk * 2)
 
 
+
+    else:
+
+        return None
+
+
+
+
+
     return {
+
         "direction": analysis["direction"],
+
         "entry": round(entry, 2),
+
         "sl": round(sl, 2),
+
         "tp": round(tp, 2),
+
         "rr": "1:2",
-        "reason": analysis["reason"]
+
+        "score": analysis.get(
+            "score",
+            0
+        ),
+
+        "reason": analysis.get(
+            "reason",
+            []
+        )
+
     }
