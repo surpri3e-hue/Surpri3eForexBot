a5184ebac9360dfa5b20680fb7a90a2ca8effe63
surpri3e-hue <surpri3e@gmail.com>
2026-07-15 00:18:10 +0330

    Update admin_tools.py

--- admin_tools.py
+++ admin_tools.py
@@ -1,12 +1,16 @@
 from settings import get_setting, set_setting
 from database import get_statistics
 from users import get_users_count
+from logs import get_logs
+
+
 
 
 
 def dashboard():
 
     stats = get_statistics()
+
 
     return f"""
 📊 DASHBOARD
@@ -36,12 +40,12 @@ def dashboard():
 {stats['profit_factor']}
 
 
+🚨 Signal:
+{get_setting('signal_status')}
+
+
 🧠 AI:
 {get_setting('ai_mode')}
-
-
-🎯 Score:
-{get_setting('minimum_score')}
 """
 
 
@@ -55,21 +59,102 @@ def toggle_signal():
     )
 
 
-    if current == "ON":
-
-        set_setting(
-            "signal_status",
-            "OFF"
-        )
-
-        return "OFF"
+    new = "OFF"
 
 
-    else:
+    if current == "OFF":
 
-        set_setting(
-            "signal_status",
-            "ON"
-        )
+        new = "ON"
 
-        return "ON"
+
+
+    set_setting(
+        "signal_status",
+        new
+    )
+
+
+    return new
+
+
+
+
+
+def toggle_channel_lock():
+
+    current = get_setting(
+        "channel_lock"
+    )
+
+
+    new = "OFF"
+
+
+    if current == "OFF":
+
+        new = "ON"
+
+
+
+    set_setting(
+        "channel_lock",
+        new
+    )
+
+
+    return new
+
+
+
+
+
+def ai_status():
+
+    return f"""
+🧠 AI SETTINGS
+
+
+Mode:
+{get_setting('ai_mode')}
+
+
+FVG:
+{get_setting('fvg_filter')}
+
+
+Liquidity:
+{get_setting('liquidity_filter')}
+
+
+BOS:
+{get_setting('bos_filter')}
+
+
+Minimum Score:
+{get_setting('minimum_score')}
+"""
+
+
+
+
+
+def logs_text():
+
+    logs = get_logs()
+
+
+    if not logs:
+
+        return "📜 No Logs"
+
+
+
+    text = "📜 LAST LOGS\n\n"
+
+
+    for time, action in logs:
+
+        text += f"🕒 {time}\n{action}\n\n"
+
+
+    return text
