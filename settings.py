17ce55da6041945d4641a23941fed06694b62ff3
surpri3e-hue <surpri3e@gmail.com>
2026-07-15 14:13:44 +0330

    Update settings.py

--- settings.py
+++ settings.py
@@ -3,6 +3,7 @@ import os
 
 SETTINGS_FILE = "settings.json"
 
+
 def init_settings():
     if not os.path.exists(SETTINGS_FILE):
         default_settings = {
@@ -16,9 +17,11 @@ def init_settings():
         with open(SETTINGS_FILE, "w") as f:
             json.dump(default_settings, f, indent=2)
 
+
 def get_settings():
     with open(SETTINGS_FILE, "r") as f:
         return json.load(f)
+
 
 def save_settings(settings):
     with open(SETTINGS_FILE, "w") as f:
